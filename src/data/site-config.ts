5ce03591378f640fbc5abf3ef369856312283d19
Mohsin M <mohsinm@MacBookAir.fritz.box>
2025-02-28 22:51:21 +0100

    fixing errors

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -97,7 +97,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        text: "Software Quality Assurance Engineer with 9+ years of professionalindustry experience. Strong abilities in Quality Assurance, Requirement Analysis, Test Plan, Test Execution, Manual && Automation Testing,Test Management, Reporting andTracking, Testing Maintenance, Team Collaboration and QA reports. Feel free to explore some of my coding endeavors on <a href='https://github.com/aheermohsinse'>GitHub</a> or follow me on <a href='https://twitter.com/'>Twitter/X</a>.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
